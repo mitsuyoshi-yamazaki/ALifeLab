131ab5c17cdae60e64d851614dbd6cbde2b749f6
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-01-14 13:11:31 +0900

    index.jsを追加

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
+    index: './src/pages/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
