ecdd40c91c094bae82d1bf5c59c33e6e47015b32
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-02-15 21:08:50 +0900

    スクリーンショットボタンを配置

---
+++ src/pages/machines_and_tapes_ex2/phisics_object.ts
@@ -0,0 +1,7 @@
+import p5 from "p5"
+import { Vector } from "../../classes/physics"
+import { Color } from "../../classes/color"
+
+class PhisicsObject {
+  // TODO: Machinesが結合した時などにひとつのオブジェクトとして扱うためのクラス
+}
