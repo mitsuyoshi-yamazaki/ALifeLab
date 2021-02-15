7225c1ab3f5a718067630fb9d98daf2ab33918c3
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-02-15 23:40:00 +0900

    不要なファイルを削除

--- src/pages/machines_and_tapes_ex2/phisics_object.ts
+++
@@ -1,7 +0,0 @@
-import p5 from "p5"
-import { Vector } from "../../classes/physics"
-import { Color } from "../../classes/color"
-
-class PhisicsObject {
-  // TODO: Machinesが結合した時などにひとつのオブジェクトとして扱うためのクラス
-}
