715892f966e35055b695b866e2ecc954efb27b84
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-02-15 23:38:38 +0900

    jestを追加

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// https://typescript-jp.gitbook.io/deep-dive/intro-1/jest
+module.exports = {
+  "preset": 'ts-jest',
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+}
---
+++ src/pages/machines_and_tapes_ex2/test/tape.test.ts
@@ -0,0 +1,8 @@
+import { Bit, Tape } from "../tape"
+
+test("description", () => {
+  expect(2)
+    .toBe(2)
+  expect(2)
+    .toBe(3)
+})
