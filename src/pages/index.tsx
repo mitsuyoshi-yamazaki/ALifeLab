e8bfad893ba0fd82407eb8b5804d6be4f0e6d243
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-02-17 16:11:37 +0900

    リンクを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom"
 const App = () => {
   return (
     <div>
-      <a href="/src/pages/meta_generative_art/index.html">Meta Generative Art</a>
+      <a href="src/pages/meta_generative_art/index.html">Meta Generative Art</a>
       <br />
-      <a href="/src/pages/machines_and_tapes/index.html">Extended Machines and Tapes</a>
+      <a href="src/pages/machines_and_tapes/index.html">Extended Machines and Tapes</a>
       <br />
-      <a href="/src/pages/machines_and_tapes_ex2/index.html">Extended Machines and Tapes v2</a>
+      <a href="src/pages/machines_and_tapes_ex2/index.html">Extended Machines and Tapes v2</a>
     </div>
   )
 }
