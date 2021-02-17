9e27da3ddcfed8ef491717a454816bf2bf220b12
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-02-17 16:45:11 +0900

    temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Helmet } from "react-helmet"
 
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <head prefix="og: http://ogp.me/ns#">test hoge</head>
+          <meta property="og:title" content="ALifes and Generative Arts" />
+          <meta property="og:description" content="" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image"
+            content="https://mitsuyoshi-yamazaki.github.io/ALifeGameJam2019/resources/docs/image001.png" />
+          <meta property="og:url" content="https://mitsuyoshi-yamazaki.github.io/ALifeGameJam2019/index.html" />
+
+      </Helmet>
       <a href="src/pages/meta_generative_art/index.html">Meta Generative Art</a>
       <br />
       <a href="src/pages/machines_and_tapes/index.html">Extended Machines and Tapes</a>
