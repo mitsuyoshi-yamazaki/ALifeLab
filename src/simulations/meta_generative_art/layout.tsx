e1346d166d4ada16cc46a7b2390f361811b0dd01
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-03-23 00:35:02 +0900

    各ページにbreadcrumbsを追加

--- src/simulations/meta_generative_art/layout.tsx
+++ src/simulations/meta_generative_art/layout.tsx
@@ -1,11 +1,13 @@
 import p5 from "p5"
 import React from "react"
 import ReactDOM from "react-dom"
+import { Breadcrumbs } from "../../react-components/breadcrumbs"
 import { main } from "./source"
 
 const App = () => {
   return (
     <div>
+      <Breadcrumbs />
       <div id="canvas-parent"></div>
     </div>
   )
--- src/simulations/meta_generative_art_v2/layout.tsx
+++ src/simulations/meta_generative_art_v2/layout.tsx
@@ -1,12 +1,14 @@
 import p5 from "p5"
 import React from "react"
 import ReactDOM from "react-dom"
+import { Breadcrumbs } from "../../react-components/breadcrumbs"
 import { ScreenShotButton } from "../../react-components/screenshot_button"
 import { main, getTimestamp } from "./source"
 
 const App = () => {
   return (
     <div>
+      <Breadcrumbs />
       <div id="canvas-parent"></div>
       <ScreenShotButton getTimestamp={() => getTimestamp()} getDescription={() => document.location.search}/>
     </div>
