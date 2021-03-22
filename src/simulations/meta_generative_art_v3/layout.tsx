ab17c89c3d8f05de6b1bac120a6d3e77b060d7dc
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-03-22 15:41:41 +0900

    ファイルを追加

---
+++ src/simulations/meta_generative_art_v3/layout.tsx
@@ -0,0 +1,17 @@
+import p5 from "p5"
+import React from "react"
+import ReactDOM from "react-dom"
+import { ScreenShotButton } from "../../react-components/screenshot_button"
+import { main, getTimestamp } from "./source"
+
+const App = () => {
+  return (
+    <div>
+      <div id="canvas-parent"></div>
+      <ScreenShotButton getTimestamp={() => getTimestamp()} getDescription={() => document.location.search}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
+const sketch = new p5(main)
