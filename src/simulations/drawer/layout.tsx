bb7b8c86aceaf3db1a21220b6f34a0a4956b8076
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-04-19 01:42:23 +0900

    説明を追加

--- src/simulations/drawer/layout.tsx
+++ src/simulations/drawer/layout.tsx
@@ -27,13 +27,11 @@ const App = () => {
         <p>
           L-Systemで幾何学図形を描画します。<br />
           描画する線分に衝突判定を入れたことで、交差する線なしに紙面を埋める図形を描画します。<br />
-          <ul style={{paddingLeft: "2rem"}}>
-            <li>
-              リロードの度に表示される図形のルール一覧: {linkOf("rule_example.ts")}
-            </li>
-            <li>
-              設定可能なURLパラメータ一覧: {linkOf("constants.ts")}
-            </li>
+          URLパラメータに{'"'}system.run=1{'"'}を設定することでランダムな図形を自動探索します。<br />
+          また{'"'}system.auto_download=1{'"'}を設定することで探索した画像を自動的に保存します。<br />
+          <ul style={{ paddingLeft: "2rem" }}>
+            <li>リロードの度に表示される図形のルール一覧: {linkOf("rule_example.ts")}</li>
+            <li>設定可能なURLパラメータ一覧: {linkOf("constants.ts")}</li>
           </ul>
         </p>
       </div>
