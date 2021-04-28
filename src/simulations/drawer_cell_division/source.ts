55a6ccfc3bcb8bf624d0a8414f8ab758c9ba83b9
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-04-29 06:43:55 +0900

    ファイルを追加

---
+++ src/simulations/drawer_cell_division/source.ts
@@ -0,0 +1,25 @@
+import p5 from "p5"
+import { defaultCanvasParentId } from "../../react-components/default_canvas_parent_id"
+
+let t = 0
+const canvasId = "canvas"
+const fieldSize = 600
+
+export const main = (p: p5): void => {
+  p.setup = () => {
+    const canvas = p.createCanvas(fieldSize, fieldSize)
+    canvas.id(canvasId)
+    canvas.parent(defaultCanvasParentId)
+
+    p.background(0, 0xFF)
+  }
+
+  p.draw = () => {
+    p.background(0, 0xFF)
+    t += 1
+  }
+}
+
+export const getTimestamp = (): number => {
+  return t
+}
