1d9596ab17ccb5aae28380c2c79ed8d5820247dd
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-05-05 14:31:28 +0900

    各ページに必要なファイルを追加

---
+++ src/pages/gallery/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { ThemeProvider } from "@material-ui/styles"
+import { defaultTheme } from "../../react-components/default_theme"
+
+const App = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    </ThemeProvider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
