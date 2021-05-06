5b1002e546ac496d8a3d6c0b51f5ff16f12e56ca
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-05-06 12:03:17 +0900

    説明を追加

--- src/simulations/hex_cellular_automata_autosearch/layout.tsx
+++ src/simulations/hex_cellular_automata_autosearch/layout.tsx
@@ -1,10 +1,20 @@
 import p5 from "p5"
-import React from "react"
+import React, { CSSProperties } from "react"
 import ReactDOM from "react-dom"
 import { DetailPage, ScreenshotButtonDefault } from "../../react-components/lab/detail_page"
 import { main, getTimestamp } from "./source"
 
 const App = () => {
+  const descriptionStyle: CSSProperties = {
+    margin: DetailPage.defaultContentMargin
+  }
+  const borderStyle: CSSProperties = {
+    backgroundColor: "rgba(212, 214, 245, 1.0)",
+    width: "100%",
+    height: "1px",
+    border: "none",
+  }
+
   const screenshotButton: ScreenshotButtonDefault = {
     kind: "default",
     getTimestamp,
@@ -12,6 +22,14 @@ const App = () => {
   }
   return (
     <DetailPage screenshotButtonType={screenshotButton}>
+      <div style={descriptionStyle}>
+        <h2>{document.title}</h2>
+        <hr style={borderStyle}></hr>
+        <p>
+          セルオートマトンのルール自動探索<br />
+          
+        </p>
+      </div>
     </DetailPage>
   )
 }
