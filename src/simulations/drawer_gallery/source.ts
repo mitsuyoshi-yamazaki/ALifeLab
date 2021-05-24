5ab0937dcfd81ed651a36557e4f904b1ff9e6ab8
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-05-24 19:56:33 +0900

    ルール切り替えを滑らかに行うよう変更

--- src/simulations/drawer_gallery/source.ts
+++ src/simulations/drawer_gallery/source.ts
@@ -1,26 +1,12 @@
 import p5 from "p5"
-import { Vector } from "../../classes/physics"
-import { random } from "../../classes/utilities"
-import { ImmortalModel, RuleDescription } from "../drawer/model"
 import { defaultCanvasParentId } from "../../react-components/common/default_canvas_parent_id"
-import { VanillaLSystemRule } from "../drawer/vanilla_lsystem_rule"
+import { Drawer } from "./drawer"
 import { exampleRules } from "../drawer/rule_examples"
-import { Downloader } from "../drawer/downloader"
 
 let t = 0
-const executionInterval = 1
 const canvasId = "canvas"
 const fieldSize = 600 // TODO: 決める
-
-const maxLineCount = 5000
-const mutationRate = 0
-const lineLengthType = 0
-const colorTheme = "grayscale"
-const fixedStartPoint = false
-
-let rules = [...exampleRules]
-let currentModel = createModel()
-const downloader = new Downloader()
+const drawer = new Drawer(fieldSize, exampleRules)
 
 export const canvasWidth = fieldSize
 
@@ -34,86 +20,7 @@ export const main = (p: p5): void => {
   }
 
   p.draw = () => {
-    if (downloader.isSaving === true) {
-      return
-    }
-
-    p.background(0x0, 0xFF)
-
-    if (t % executionInterval === 0) {
-      currentModel.execute()
-    }
-    currentModel.draw(p, false)
-
-    if (currentModel.result != null) {
-      const result = currentModel.result
-      const status = `${result.status.numberOfLines} lines, ${result.status.numberOfNodes} nodes`
-      const rules = result.rules.sort((lhs: RuleDescription, rhs: RuleDescription) => {
-        if (lhs.numberOfDrawers === rhs.numberOfDrawers) {
-          return 0
-        }
-        return lhs.numberOfDrawers < rhs.numberOfDrawers ? 1 : -1
-      })
-      const ruleDescription = rules.reduce((r, rule) => `${r}\n${rule.numberOfDrawers} drawers: ${rule.rule}`, "")
-      console.log(`completed at ${t} (${result.t} steps, ${result.reason}, ${status}) ${result.description}\n${ruleDescription}`)
-      currentModel = createModel()
-    }
-
-    drawRule(p, currentModel.lSystemRules[0].encoded)
-
+    drawer.next(p)
     t += 1
   }
-}
-
-export const saveCurrentState = (): void => {
-  const result = currentModel.currentResult("Running")
-  const rules = result.rules.sort((lhs: RuleDescription, rhs: RuleDescription) => {
-    if (lhs.numberOfDrawers === rhs.numberOfDrawers) {
-      return 0
-    }
-    return lhs.numberOfDrawers < rhs.numberOfDrawers ? 1 : -1
-  })
-  downloader.save("", rules, t, result.t)
-}
-
-function createModel(): ImmortalModel {
-  const rule = nextRule()
-  const rules: VanillaLSystemRule[] = []
-  try {
-    rules.push(new VanillaLSystemRule(rule))
-  } catch (error) {
-    alert(`Invalid rule ${rule}`)
-    throw error
-  }
-  const model = new ImmortalModel(
-    new Vector(fieldSize, fieldSize),
-    maxLineCount,
-    rules,
-    mutationRate,
-    lineLengthType,
-    colorTheme,
-    fixedStartPoint,
-  )
-  model.showsBorderLine = false
-  model.lineCollisionEnabled = true
-  model.quadtreeEnabled = true
-  model.concurrentExecutionNumber = 100 // TODO: 調整する
-
-  return model
-}
-
-function nextRule(): string {
-  if (rules.length === 0) {
-    rules = [...exampleRules]
-  }
-  return rules.splice(Math.floor(random(rules.length)), 1)[0]
-}
-
-function drawRule(p: p5, rule: string): void {
-  const textSize = 12
-  const margin = 10
-
-  p.fill(0xFF)
-  p.textSize(textSize)
-  p.text(rule, margin, fieldSize - margin)
 }
