db86c0375136d39e65330babe5d5f0c237b7547f
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-10-24 18:25:52 +0900

    canvas範囲外のタップに反応する問題を修正

--- src/simulations/la_interactive/source.ts
+++ src/simulations/la_interactive/source.ts
@@ -2,13 +2,11 @@ import p5 from "p5"
 import { defaultCanvasParentId } from "../../react-components/common/default_canvas_parent_id"
 import { Drawer } from "./drawer"
 import { exampleRules } from "../drawer/rule_examples"
-import { isFullScreen, toggleFullscreen } from "../../classes/utilities"
 import { Vector } from "../../classes/physics"
 import { VanillaLSystemRule } from "../drawer/vanilla_lsystem_rule"
 import { constants } from "../drawer/constants"
 
-const fullscreenEnabled = false as boolean
-
+let t = 0
 const canvasId = "canvas"
 const screenSize = new Vector(window.screen.width, window.screen.height)
 const isPortrait = ((): boolean => {
@@ -43,15 +41,19 @@ export const main = (p: p5): void => {
 
   p.draw = () => {
     drawer.next(p)
+
+    t += 1
   }
 
   p.mousePressed = () => {
-    if (fullscreenEnabled === true && isFullScreen() !== true) {
-      toggleFullscreen(canvasId)
+    const position = new Vector(p.mouseX, p.mouseY)
+    if (position.x < 0 || position.x > fieldSize.x || position.y < 0 || position.y > fieldSize.y) {
       return
     }
-
-    const position = new Vector(p.mouseX, p.mouseY)
     drawer.didReceiveTouch(position)
   }
+}
+
+export const getTimestamp = (): number => {
+  return t
 }
