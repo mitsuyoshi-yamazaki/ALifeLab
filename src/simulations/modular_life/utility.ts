691593c44c74f33d681122c43282fa3e3d92fa68
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-02-11 14:07:06 +0900

    isNearToが逆の結果を出す問題を修正

--- src/simulations/modular_life/utility.ts
+++ src/simulations/modular_life/utility.ts
@@ -1,5 +1,5 @@
 import { Vector } from "../../classes/physics"
 
 export const isNearTo = (position1: Vector, position2: Vector): boolean => {
-  return position1.dist(position2) > 1
+  return position1.dist(position2) <= 1
 }
