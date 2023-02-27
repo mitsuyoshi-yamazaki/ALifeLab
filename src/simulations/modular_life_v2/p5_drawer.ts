9c05095f238657fb64fdc97b473a773dcab06a62
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-02-28 00:24:21 +0900

    生命の描画処理を追加

--- src/simulations/modular_life_v2/p5_drawer.ts
+++ src/simulations/modular_life_v2/p5_drawer.ts
@@ -1,10 +1,13 @@
 import p5 from "p5"
 import { strictEntries } from "../../classes/utilities"
-import { Terrain } from "./terrain"
+import { Terrain, TerrainCell } from "./terrain"
 import { World } from "./world"
 
 type DrawModeMaterial = {
   readonly case: "material"
+}
+type DrawModeLife = {
+  readonly case: "life"
 }
 type DrawModeEnergy = {
   readonly case: "energy"
@@ -16,10 +19,12 @@ type DrawModeStatus = {
   readonly case: "status"
   readonly text: string
 }
-type DrawMode = DrawModeMaterial | DrawModeEnergy | DrawModeHeat | DrawModeStatus
+type DrawMode = DrawModeMaterial | DrawModeLife | DrawModeEnergy | DrawModeHeat | DrawModeStatus
 type DrawModes = DrawMode["case"]
+type InnerCellDrawModes = DrawModeMaterial["case"] | DrawModeLife["case"] | DrawModeEnergy["case"] | DrawModeHeat["case"]
 type GenericDrawMode<T extends DrawModes> = T extends "material" ? DrawModeMaterial :
   T extends "energy" ? DrawModeEnergy :
+  T extends "life" ? DrawModeLife :
   T extends "heat" ? DrawModeHeat :
   T extends "status" ? DrawModeStatus :
   never
@@ -47,50 +52,57 @@ export class P5Drawer {
   public drawWorld(p: p5, world: World): void {
     p.background(0x00)
 
-    if (this.drawMode["energy"] != null) {
-      this.drawEnergy(p, world.terrain)
+    const drawModes = this.drawModes
+    const drawTargets: { [Draw in InnerCellDrawModes]: boolean } = {
+      material: drawModes.includes("material"),
+      life: drawModes.includes("life"),
+      energy: drawModes.includes("energy"),
+      heat: drawModes.includes("heat"),
     }
-    if (this.drawMode["material"] != null) {
-      this.drawMaterial(p, world)
-    }
-    if (this.drawMode["heat"] != null) {
-      this.drawHeat(p, world.terrain)
-    }
+
+    world.terrain.cells.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        this.drawTerrainCell(p, cell, x, y, drawTargets)
+      })
+    })
+
     if (this.drawMode["status"] != null) {
       this.drawStatus(p, world, this.drawMode["status"])
     }
   }
 
-  private drawMaterial(p: p5, world: World): void {
-    p.noStroke()
-    p.fill(0xFF)
-    p.circle(100, 100, 100) // TODO:
-  }
-
-  private drawEnergy(p: p5, terrain: Terrain): void {
+  private drawTerrainCell(p: p5, cell: TerrainCell, x: number, y: number, drawTargets: { [Draw in InnerCellDrawModes]: boolean }): void {
     const cellSize = this.cellSize
-    const energyMeanAmount = 10 // FixMe:
 
-    terrain.cells.forEach((row, y) => {
-      row.forEach((cell, x) => {
-        const alpha = Math.floor((cell.energy / energyMeanAmount) * 0x80)
-        p.fill(0xFF, 0xFF, 0x00, alpha)
-        p.square(x * cellSize, y * cellSize, cellSize)
+    if (drawTargets.material === true) {
+      // TODO:
+    }
+
+    if (drawTargets.life === true) {
+      cell.hull.forEach(hull => {
+        p.noStroke()
+        p.fill(0xFF, 0xC0)
+        p.ellipse(x * cellSize, y * cellSize, cellSize, cellSize)
       })
-    })
-  }
+    }
 
-  private drawHeat(p: p5, terrain: Terrain): void {
-    const cellSize = this.cellSize
-    const heatMeanAmount = 10 // FixMe: 
+    if (drawTargets.energy === true) {
+      const energyMeanAmount = 10 // FixMe:
 
-    terrain.cells.forEach((row, y) => {
-      row.forEach((cell, x) => {
-        const alpha = Math.floor((cell.heat / heatMeanAmount) * 0x80)
-        p.fill(0xFF, 0x00, 0x00, alpha)
-        p.square(x * cellSize, y * cellSize, cellSize)
-      })
-    })
+      p.noStroke()
+      const alpha = Math.floor((cell.amount.energy / energyMeanAmount) * 0x80)
+      p.fill(0xFF, 0xFF, 0x00, alpha)
+      p.rect(x * cellSize, y * cellSize, cellSize, cellSize)
+    }
+
+    if (drawTargets.heat === true) {
+      const heatMeanAmount = 10 // FixMe: 
+
+      p.noStroke()
+      const alpha = Math.floor((cell.heat / heatMeanAmount) * 0x80)
+      p.fill(0xFF, 0x00, 0x00, alpha)
+      p.rect(x * cellSize, y * cellSize, cellSize, cellSize)
+    }
   }
 
   private drawStatus(p: p5, world: World, status: DrawModeStatus): void {
