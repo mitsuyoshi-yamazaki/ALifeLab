d09590800298a82c5ab531ea6a4081072928ebb0
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-02-27 19:14:53 +0900

    祖先種の種類を定義

--- src/simulations/modular_life_v2/ancestor/ancestor.ts
+++ src/simulations/modular_life_v2/ancestor/ancestor.ts
@@ -1,5 +1,29 @@
 import { SourceCode } from "../module/source_code"
 
-export const createAncestor = (code: SourceCode): unknown => {  // TODO:
+/// 生命として成立する最小の祖先種
+export const createMinimumAncestor = (code: SourceCode): unknown => {  // TODO:
+  /*
+  - 計算機 x 1
+  - 外殻 x 1
+  - 推進器 x 1
+   */
+  throw "not implemented"
+}
+
+/// 自己複製できる最小の祖先種
+export const createMinimumSelfReproductionAncestor = (code: SourceCode): unknown => {  // TODO:
+  /*
+  - 計算機 x 1
+  - 物質輸送口 x 1
+  - 外殻 x n
+  - 推進器 x 1
+  - 物質変換器 x n
+  - 組み立て機 x 1
+  */
+  throw "not implemented"
+}
+
+/// 自身より複雑な子孫を組み立てられる祖先種
+export const createGeneralAssemblerAncestor = (code: SourceCode): unknown => {  // TODO:
   throw "not implemented"
 }
