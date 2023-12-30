682fe44c45329ad8c8e238e8a5b0d134ee6e4c65
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-12-30 20:38:48 +0900

    静的リソースの配置試験

--- apps/vercel/src/app/page.tsx
+++ apps/vercel/src/app/page.tsx
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>ALife Lab</h1>
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        src="/static/blindpainter_001.png"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
