d30ac6a5b1fb4350f6c74041f7ac7a9df760b2e1
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-12-22 17:47:10 +0900

    現在描画中のルールを表示

--- src/simulations/drawer_sandbox/layout.tsx
+++ src/simulations/drawer_sandbox/layout.tsx
@@ -1,12 +1,29 @@
 import p5 from "p5"
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { DetailPage } from "../../react-components/lab/detail_page"
-import { main } from "./source"
+import { changeRule, main } from "./source"
 
 const App = () => {
+  const [rule, setRule] = useState("A:-88,A,-152,A")
+
+  const ruleDidChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedRule = event.target.value
+    setRule(updatedRule)
+    changeRule(updatedRule)
+  }
+
   return (
     <DetailPage screenshotButtonType={{ kind: "none" }}>
+      <div>
+        <label>Rule:</label>
+        <input
+          type="text"
+          value={rule}
+          onChange={ruleDidChange}
+          style={{width: "100%"}}
+        />
+      </div>
     </DetailPage>
   )
 }
