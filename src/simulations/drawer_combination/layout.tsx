e187865fff1d75515eca5b08de4ef83cc35bccfe
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-12-31 14:34:43 +0900

    パターン一覧表示を追加

--- src/simulations/drawer_combination/layout.tsx
+++ src/simulations/drawer_combination/layout.tsx
@@ -1,8 +1,51 @@
 import p5 from "p5"
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { DetailPage, ScreenshotButtonDefault } from "../../react-components/lab/detail_page"
 import { main, getTimestamp } from "./source"
+
+type LocalPattern = {
+  readonly imagePath: string
+  readonly rule: string
+}
+
+const PatternGallery = (props: {patterns: LocalPattern[], didSelectRule: (rule: string) => void}) => {
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0)
+
+  const didSelectRule = (index: number, pattern: LocalPattern) => {
+    console.log(`${index}, selected ${pattern.rule}`)
+
+    setSelectedImageIndex(index)
+    props.didSelectRule(pattern.rule)
+  }
+
+  return (
+    <div style={{ overflowX: "scroll", whiteSpace: "nowrap", padding: "10px" }}>
+      {props.patterns.map((pattern, index) => (
+        <div
+          key={index}
+          onClick={() => didSelectRule(index, pattern)}
+          style={{
+            display: "inline-block",
+            marginRight: "10px",
+            outline: selectedImageIndex === index ? "3px solid blue" : "none",
+            cursor: "pointer",
+          }}
+        >
+          <img
+            src={pattern.imagePath}
+            style={{
+              width: "100px",
+              height: "100px",
+              objectFit: "cover",
+              borderRadius: "8px",
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
 
 const App = () => {
   const screenshotButton: ScreenshotButtonDefault = {
@@ -10,8 +53,17 @@ const App = () => {
     getTimestamp,
     getDescription: () => document.location.search
   }
+  const patterns: LocalPattern[] = [
+    { imagePath: "", rule: "A:2A" },
+    { imagePath: "", rule: "A:10A" },
+    { imagePath: "", rule: "A:20A" },
+    { imagePath: "", rule: "A:30A" },
+    { imagePath: "", rule: "A:40A" },
+  ]
+
   return (
     <DetailPage screenshotButtonType={screenshotButton}>
+      <PatternGallery patterns={patterns} didSelectRule={rule => console.log(`selected ${rule}`)} />
     </DetailPage>
   )
 }
