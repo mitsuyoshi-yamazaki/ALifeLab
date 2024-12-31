f3a0e8b6d728f79ca6bb44f0b4c5aae330647d06
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-12-31 15:55:55 +0900

    葉のルール選択UIを追加

--- src/simulations/drawer_combination/layout.tsx
+++ src/simulations/drawer_combination/layout.tsx
@@ -9,26 +9,17 @@ type LocalPattern = {
   readonly rule: string
 }
 
-const PatternGallery = (props: {patterns: LocalPattern[], didSelectRule: (rule: string) => void}) => {
-  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0)
-
-  const didSelectRule = (index: number, pattern: LocalPattern) => {
-    console.log(`${index}, selected ${pattern.rule}`)
-
-    setSelectedImageIndex(index)
-    props.didSelectRule(pattern.rule)
-  }
-
+const PatternGallery = (props: {patterns: LocalPattern[], selectingPatternIndex: number, didSelectRule: (index: number) => void}) => {
   return (
     <div style={{ overflowX: "scroll", whiteSpace: "nowrap", padding: "10px" }}>
       {props.patterns.map((pattern, index) => (
         <div
           key={index}
-          onClick={() => didSelectRule(index, pattern)}
+          onClick={() => props.didSelectRule(index)}
           style={{
             display: "inline-block",
             marginRight: "10px",
-            outline: selectedImageIndex === index ? "3px solid blue" : "none",
+            outline: index === props.selectingPatternIndex ? "3px solid blue" : "none",
             cursor: "pointer",
           }}
         >
@@ -47,23 +38,43 @@ const PatternGallery = (props: {patterns: LocalPattern[], didSelectRule: (rule: 
   )
 }
 
+const stringRepresentation = (rule: string): string => {
+  return rule.replace(/,/g, "").replace(/;/g, ", ").replace(/:/g, "→")
+}
+
 const App = () => {
   const screenshotButton: ScreenshotButtonDefault = {
     kind: "default",
     getTimestamp,
     getDescription: () => document.location.search
   }
-  const patterns: LocalPattern[] = [
-    { imagePath: "../src/simulations/drawer_combination/images/001.png", rule: "A:0,A" },
-    { imagePath: "../src/simulations/drawer_combination/images/002.png", rule: "A:2,A" },
-    { imagePath: "../src/simulations/drawer_combination/images/003.png", rule: "A:4,A" },
-    { imagePath: "../src/simulations/drawer_combination/images/004.png", rule: "A:9,A,-18,A" },
-    { imagePath: "../src/simulations/drawer_combination/images/005.png", rule: "A:20,A,-21,B;B:0,A" },
+  const stemPatterns: LocalPattern[] = [
+    { imagePath: "../src/simulations/drawer_combination/images/001.png", rule: "A:0,A,0,Z" },
+    { imagePath: "../src/simulations/drawer_combination/images/002.png", rule: "A:2,A,0,Z" },
+    { imagePath: "../src/simulations/drawer_combination/images/003.png", rule: "A:4,A,0,Z" },
+    { imagePath: "../src/simulations/drawer_combination/images/004.png", rule: "A:9,A,-18,A,0,Z" },
+    { imagePath: "../src/simulations/drawer_combination/images/005.png", rule: "A:20,A,-21,B;B:0,A,0,Z" },
   ]
+  const leafPatterns: LocalPattern[] = [
+    { imagePath: "", rule: "Z:0,Y;Y:-101,Y,101,Y,5,Y" },
+  ]
+
+  const [selectedStemIndex, setSelectedStemIndex] = useState<number>(0)
+  const [selectedLeafIndex, setSelectedLeafIndex] = useState<number>(0)
+
+  const stemRule = stemPatterns[selectedStemIndex].rule
+  const leafRule = leafPatterns[selectedLeafIndex].rule
+  const constructedRule = stemRule + ";" + leafRule
+
+  console.log(`Rule changed to: ${stringRepresentation(constructedRule)}`)
 
   return (
     <DetailPage screenshotButtonType={screenshotButton}>
-      <PatternGallery patterns={patterns} didSelectRule={rule => console.log(`selected ${rule}`)} />
+      <h2>パターンを選択する</h2>
+      <h3>{`幹：${stringRepresentation(stemRule)}`}</h3>
+      <PatternGallery patterns={stemPatterns} selectingPatternIndex={selectedStemIndex} didSelectRule={index => setSelectedStemIndex(index)} />
+      <h3>{`葉：${stringRepresentation(leafRule)}`}</h3>
+      <PatternGallery patterns={leafPatterns} selectingPatternIndex={selectedLeafIndex} didSelectRule={index => setSelectedLeafIndex(index)} />
     </DetailPage>
   )
 }
